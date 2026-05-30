f368c7c8d63dbd665803911dea732cb7f9866a52
techaphat <techaphat@outlook.com>
2026-05-30 15:40:40 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,6 @@
-self.addEventListener('install', event => {
-  self.skipWaiting();
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
-});
+// No-cache service worker: keeps GitHub Pages from serving old broken files.
+self.addEventListener('install', event => { self.skipWaiting(); });
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
-      .then(() => self.registration.unregister())
-      .then(() => self.clients.claim())
-  );
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.clients.claim()));
 });
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
-});
+self.addEventListener('fetch', event => { event.respondWith(fetch(event.request)); });
