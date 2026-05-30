ab16e91657983309ca5514921acd52469f7a4c2b
techaphat <techaphat@outlook.com>
2026-05-30 15:32:58 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,30 +1,15 @@
-const CACHE_NAME = 'm1-good-good-jsonp-v1';
-const CORE_ASSETS = [
-  './',
-  './index.html',
-  './dashboard.html',
-  './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
-];
-
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(CORE_ASSETS)));
   self.skipWaiting();
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
 });
-
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.registration.unregister())
+      .then(() => self.clients.claim())
   );
-  self.clients.claim();
 });
-
 self.addEventListener('fetch', event => {
-  const req = event.request;
-  if (req.url.includes('script.google.com') || req.url.includes('script.googleusercontent.com')) {
-    event.respondWith(fetch(req));
-    return;
-  }
-  event.respondWith(fetch(req).catch(() => caches.match(req)));
+  event.respondWith(fetch(event.request));
 });
